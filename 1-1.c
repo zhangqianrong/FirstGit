3d8500d2aa100fdb7d5b0635845ca66928307f39
zhangqianrong <rabsic_r@163.com>
2015-11-16 16:18:17 +0800

    add 1-1.c 1-2.c

---
+++ 1-1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int dogs;
+	printf("How many dogs do you have?\n");
+	scanf("%d",&dogs);
+	printf("So you have %d dog :\n",dogs);
+	return 0;
+
+
+
+
+}
+
---
+++ 1-2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("Concerte contains gravel and cenment.\n");
+	return 0;
+
+
+
+
+}
